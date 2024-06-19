e4edd371d56ee9cb270fcd0e0c1c8ffda8a37963
Michael Kuhn <64441753+thundamike@users.noreply.github.com>
2024-06-19 18:22:01 -0500

    Update settings.py

--- api/settings.py
+++ api/settings.py
@@ -27,10 +27,9 @@ load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "Oauth/static")]
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 STATIC_URL = "static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "Oauth/staticfiles")
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
